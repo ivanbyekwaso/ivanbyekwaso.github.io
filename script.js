46f41cc7dc3a62073e2e47c5b21f11f990a4f872
ivanbyekwaso <gpotrade98@gmail.com>
2026-04-02 00:12:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-// empty for now (safe)
+function revealElements() {
+  document.querySelectorAll(".reveal").forEach(el => {
+    const top = el.getBoundingClientRect().top;
+    if (top < window.innerHeight - 80) {
+      el.classList.add("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", revealElements);
+window.addEventListener("load", revealElements);
