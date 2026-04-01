9f1aa0e55ce7b4cf15715a8c77f392913a61a815
ivanbyekwaso <gpotrade98@gmail.com>
2026-04-02 00:07:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-function revealElements() {
-  document.querySelectorAll(".reveal").forEach(el => {
-    const top = el.getBoundingClientRect().top;
-    if (top < window.innerHeight - 80) {
-      el.classList.add("active");
-    }
-  });
-}
-
-window.addEventListener("scroll", revealElements);
-window.addEventListener("load", revealElements);
+// empty for now (safe)
